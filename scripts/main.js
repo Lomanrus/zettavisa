7a653421da088373b66893c38a6ecee6139d6d1c
Александр Тихонов <oren.andrey@gmail.com>
2020-08-10 21:42:29 +0500

    Добавлен выпадающий список городов для главной страницы

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,25 @@ $(function () {
     $('#modal-form__phone').mask("8(999) 999-99-99")
 })
 
+function citySelect() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 $(document).ready(function () {
 
     $('.news-list').slick({
